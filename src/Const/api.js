65c908302c24ba7364bda1614b9fbacc391277cb
Rofiqur Rohman <64766958+rofiqurrohman@users.noreply.github.com>
2021-02-17 15:50:17 +0700

    Update api.js

--- src/Const/api.js
+++ src/Const/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    // baseURL: 'https://cors-anywhere.herokuapp.com/http://jendela.data.kemdikbud.go.id/api/index.php',
-    baseURL: 'http://47.74.71.135:8080/api',
+    baseURL: 'https://api-sekolahku.now.sh/api',
 });
